3fdfd32f450a2bf2bc42a9a79af5012688437994
George G <ggogarten@gmail.com>
2016-06-22 16:08:06 -0400

    webview complete

--- Webviews/ViewController.swift
+++ Webviews/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var webview: UIWebView!
     
@@ -17,21 +17,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-   
-        var url = NSURL(string: "https://www.google.com")
         
-        var request = NSURLRequest(URL: url!)
         
-        webview.loadRequest(request)
-    
-    
+        
+        /*
+         
+         var url = NSURL(string: "https://www.google.com")
+         
+         var request = NSURLRequest(URL: url!)
+         
+         webview.loadRequest(request)
+         
+         */
+        
+        
+        var html = "<html><body><h1>MY Page</h1><p>This is my web page.</p></body></html>"
+        
+        webview.loadHTMLString(html, baseURL: nil)
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
